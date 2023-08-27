432ee7e09e17f40912608f0c44d2a03c0fe67a1b
Olga Kudryavtseva <105752494+Encephalon1@users.noreply.github.com>
2023-08-27 12:59:27 +0300

    Make comments

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def calculator():
     mathem = input('Введите математическое выражение через пробел (например, 2 + 2): ')
     mathem_list = list(mathem.split(' '))
-
+    
+    # Для правильного порядка действий
     result = 0
     num2 = 0
     num3 = 0
@@ -10,6 +11,7 @@ def calculator():
     num = 0
 
     for i in mathem_list:
+        # Сначала переводим строку в число, заодно отлавливаем ошибку
         if i != '+' and i != '-' and i != '*' and i != '/':
             try:
                 i = int(i)
@@ -17,6 +19,7 @@ def calculator():
                 print('Некорректное выражение!')
                 return
 
+        # Здесь сдвигаемся по выражению слева направо, по одному символу
         if type(i) == int:
             if result == 0:
                 result = i
@@ -27,7 +30,8 @@ def calculator():
             operation1 = operation2
             operation2 = i
 
-        if i != '+' and i != '-' and i != '*' and i != '/':
+        if i != '+' and i != '-' and i != '*' and i != '/': # Чтобы не выполнялись лишние действия
+            # Правильный порядок действий
             if operation2 == '*' and operation1 != '*' and operation1 != '/':
                 num = num2 * num3
                 if operation1 == '+':
